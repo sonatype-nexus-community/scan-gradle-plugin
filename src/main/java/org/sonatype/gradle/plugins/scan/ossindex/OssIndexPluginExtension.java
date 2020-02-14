52a8e43f474273310a32a91980c9bafa2c5b6a2f
Guillermo Varela <gvarela@sonatype.com>
2020-02-14 17:37:25 -0500

    Add OSS Index support (#17)

---
+++ src/main/java/org/sonatype/gradle/plugins/scan/ossindex/OssIndexPluginExtension.java
@@ -0,0 +1,106 @@
+/*
+ * Copyright (c) 2020-present Sonatype, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.sonatype.gradle.plugins.scan.ossindex;
+
+import org.gradle.api.Project;
+
+public class OssIndexPluginExtension
+{
+  private String username;
+
+  private String password;
+
+  private boolean useCache;
+
+  private String cacheDirectory;
+
+  /**
+   * It must follow the duration format from <a href=
+   * "https://www.javadoc.io/doc/joda-time/joda-time/2.10.4/org/joda/time/Duration.html#parse-java.lang.String-">
+   * Duration</a>
+   */
+  private String cacheExpiration;
+
+  private boolean simulationEnabled;
+
+  private boolean simulatedVulnerabityFound;
+
+  public OssIndexPluginExtension(Project project) {
+    username = "";
+    password = "";
+    useCache = true;
+    cacheDirectory = "";
+    cacheExpiration = "";
+    simulationEnabled = false;
+    simulatedVulnerabityFound = false;
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public boolean isUseCache() {
+    return useCache;
+  }
+
+  public void setUseCache(boolean useCache) {
+    this.useCache = useCache;
+  }
+
+  public String getCacheDirectory() {
+    return cacheDirectory;
+  }
+
+  public void setCacheDirectory(String cacheDirectory) {
+    this.cacheDirectory = cacheDirectory;
+  }
+
+  public String getCacheExpiration() {
+    return cacheExpiration;
+  }
+
+  public void setCacheExpiration(String cacheExpiration) {
+    this.cacheExpiration = cacheExpiration;
+  }
+
+  public boolean isSimulationEnabled() {
+    return simulationEnabled;
+  }
+
+  public void setSimulationEnabled(boolean simulationEnabled) {
+    this.simulationEnabled = simulationEnabled;
+  }
+
+  public boolean isSimulatedVulnerabityFound() {
+    return simulatedVulnerabityFound;
+  }
+
+  public void setSimulatedVulnerabityFound(boolean simulatedVulnerabityFound) {
+    this.simulatedVulnerabityFound = simulatedVulnerabityFound;
+  }
+}
---
+++ src/main/java/org/sonatype/gradle/plugins/scan/ossindex/TransportBuilder.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) 2020-present Sonatype, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.sonatype.gradle.plugins.scan.ossindex;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+import org.sonatype.ossindex.service.client.internal.Version;
+import org.sonatype.ossindex.service.client.internal.VersionSupplier;
+import org.sonatype.ossindex.service.client.transport.HttpClientTransport;
+import org.sonatype.ossindex.service.client.transport.UserAgentBuilder;
+import org.sonatype.ossindex.service.client.transport.UserAgentBuilder.Product;
+import org.sonatype.ossindex.service.client.transport.UserAgentSupplier;
+
+import org.gradle.api.Project;
+import org.gradle.internal.impldep.com.google.common.annotations.VisibleForTesting;
+
+public class TransportBuilder
+{
+  public HttpClientTransport build(Project project) {
+    UserAgentSupplier userAgentSupplier = buildUserAgentSupplier(project);
+    return new HttpClientTransport(userAgentSupplier);
+  }
+
+  @VisibleForTesting
+  UserAgentSupplier buildUserAgentSupplier(Project project) {
+    return new UserAgentSupplier(new VersionSupplier().get())
+    {
+      @Override
+      protected void customize(UserAgentBuilder builder) {
+        String pluginVersion;
+
+        try (InputStream stream = getClass().getResourceAsStream("/com/sonatype/insight/client.properties")) {
+          Properties properties = new Properties();
+          properties.load(stream);
+          pluginVersion = properties.getProperty("version", Version.UNKNOWN);
+        }
+        catch (IOException e) {
+          pluginVersion = Version.UNKNOWN;
+        }
+
+        builder.product(new Product("Gradle", project.getGradle().getGradleVersion()));
+        builder.product(new Product("Gradle-Plugin", pluginVersion));
+      }
+    };
+  }
+}
