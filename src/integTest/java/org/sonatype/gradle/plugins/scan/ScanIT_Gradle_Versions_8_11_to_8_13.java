807d046695b1114ed61c0e38a2c5bf52bc34efac
Guillermo Varela <gvarela@sonatype.com>
2025-03-27 19:52:08 -0500

    Tests on Gradle 8.11, 8.12 and 8.13

---
+++ src/integTest/java/org/sonatype/gradle/plugins/scan/ScanIT_Gradle_Versions_8_11_to_8_13.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2020-present Sonatype, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.sonatype.gradle.plugins.scan;
+
+import java.util.List;
+
+import org.junit.runners.Parameterized;
+
+public class ScanIT_Gradle_Versions_8_11_to_8_13
+    extends ScanPluginIntegrationTestBase
+{
+  @Parameterized.Parameters(name = "Version: {0}")
+  public static List<String> data() {
+    return List.of("8.11.1", "8.12.1", "8.13");
+  }
+
+  public ScanIT_Gradle_Versions_8_11_to_8_13(final String gradleVersion) {
+    super(gradleVersion);
+  }
+}
